ddc8cfd2c5f4cc36aca00edc088ec6d8bd5ad8de
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-04-28 22:19:08 -0300

    Adicionando uso strictNullChecks no arquivo tsconfig.json

--- typescript/alurabank/app/ts/views/View.ts
+++ typescript/alurabank/app/ts/views/View.ts
@@ -1,11 +1,16 @@
 export abstract class View<T> {
     private _elemento: JQuery;
+    private _escapar: boolean;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar: boolean = false) {
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
 
     update(model: T): void {
+        let template = this.template(model);
+        if(this._escapar)
+          template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
         this._elemento.html(this.template(model));
     }
 
