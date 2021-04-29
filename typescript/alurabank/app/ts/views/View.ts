33c6acfc941e18385e8de9cb4fd269a89c62d498
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-04-29 20:52:00 -0300

    Criado decorator

--- typescript/alurabank/app/ts/views/View.ts
+++ typescript/alurabank/app/ts/views/View.ts
@@ -1,3 +1,4 @@
+import {logarTempoDeExecucao} from '../helpers/decorators/index';
 export abstract class View<T> {
     private _elemento: JQuery;
     private _escapar: boolean;
@@ -7,6 +8,7 @@ export abstract class View<T> {
         this._escapar = escapar;
     }
 
+    @logarTempoDeExecucao()
     update(model: T): void {
         let template = this.template(model);
         if(this._escapar)
